15fd6601812802c7a8317b1ca52e4fa23162c400
AzureAD\10800 <os.miguelparedes@gmail.com>
2024-06-01 14:39:35 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,18 +11,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+  ],
   providers: [
     Geolocation,
     NativeGeocoder,
-    { 
-      provide: RouteReuseStrategy, 
-      useClass: IonicRouteStrategy
-    }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   ],
   bootstrap: [AppComponent],
 })
-
 export class AppModule {}
